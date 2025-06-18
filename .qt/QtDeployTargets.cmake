8e477c54b0faf693da6effa5cf4fe15f4346100a
Amiran <astrakhanrussia@mail.ru>
2025-06-18 22:40:12 +0300

    Immital commit1

---
+++ .qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_appFridge_FILE /home/user/Fridge/appFridge)
+set(__QT_DEPLOY_TARGET_appFridge_TYPE EXECUTABLE)
---
+++ .qt/appFridge_qml.cmake
@@ -0,0 +1,9 @@
+
+set(target "appFridge")
+set(working_dir "/home/user/Fridge")
+set(src_and_dest_list
+    "/home/user/Fridge/Main.qml"
+    "/home/user/Fridge/Fridge/Main.qml"
+
+)
+set(timestamp_file "/home/user/Fridge/.qt/appFridge_qml.txt")
